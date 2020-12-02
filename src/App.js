9a1bb28198e705c1626519a1c318979fa88bdddd
nielvid <nielvidservices@gmail.com>
2020-12-02 13:03:50 +0100

    new-dev is showing lyrics on click of red buton

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function App() {
      </div>
     
      
-  
+
     <Footer />
     </div>
   );
--- src/component/Tracks.js
+++ src/component/Tracks.js
@@ -1,5 +1,5 @@
 import thumbs from "../img/thumbs.jpg"
-import { useState, useEffect, useRef} from "react"
+import { useState, useEffect, useRef } from "react"
 import axios from 'axios'
 import code from "../package/config"
 import Lyrics from "./Lyrics"
@@ -7,70 +7,72 @@ import Lyrics from "./Lyrics"
 
 
 
-function Tracks(props){
+function Tracks(props) {
 
-const [lyric, setLyric]= useState(null)
-let btn = useRef(null)
-
-
-
-   
-   
+    const [lyric, setLyric] = useState(null)
+    let btn = useRef(null)
 
 
-   const handleClick = (id)=>{
-  
-    id = btn.current.value
-       axios.get('https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id='+id+'&'+code.pass+code.code)
-        .then((response)=>{
 
-          setLyric(response.data.message.body.lyrics.lyrics_body)
-           
 
-        
-   })
-   }
+
+
+
+    const handleClick = (id) => {
+
+        id = btn.current.value
+        axios.get('https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + id + '&' + code.pass + code.code)
+            .then((response) => {
+
+                setLyric(response.data.message.body.lyrics.lyrics_body)
+
+
+
+            })
+    }
+
 
     return (
 
-      
+
 
 
         <div className="container">
-       
-                    
-                    {/*image-thumbnail start*/}
-                        <div className="call_to_action">
 
-                        <div className="thumbs tracks_thumbs">
-                        <img src={thumbs} alt="thumbs" />
-                        <div>
+
+            {/*image-thumbnail start*/}
+            <div className="call_to_action">
+
+                <div className="thumbs tracks_thumbs">
+                    <img src={thumbs} alt="thumbs" />
+                    <div>
                         <h4><a href="/">{props.title}</a></h4>
                         <h5 >{props.id}</h5>
-                            <h6>{props.artist}</h6>
-                       
-                        </div>
-                        
-                        </div>
-                    
+                        <h6>{props.artist}</h6>
 
-                        <div className="user-action">
-                            
-                       
-                           <button onClick= {handleClick} ref={btn} value={props.id}>Show Lyrics</button>
-                    
-                           <button><a href={props.share}>Share</a></button> 
-                        </div>
+                    </div>
 
-                        </div>
-                        <div>{lyric}</div>
-                        <hr  id="track-divider" />
-                       {/*image-thumbnail ends*/}
+                </div>
+                <Lyrics lyrics={lyric} />
 
-                    
-                    
+                <div className="user-action">
+
+
+                    <button onClick={handleClick} ref={btn} value={props.id}>Show Lyrics</button>
+
+                    <button><a href={props.share}>Share</a></button>
+                </div>
+
             </div>
-        
+
+
+            <hr id="track-divider" />
+            {/*image-thumbnail ends*/}
+
+
+
+        </div>
+
     )
 }
 export default Tracks;
